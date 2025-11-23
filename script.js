69df355768e57312e405626556bd0350b6252b31
noliforasire <118840325+noliforas@users.noreply.github.com>
2025-11-23 17:57:50 +0800

    1123大升级-测试4！

--- script.js
+++ script.js
@@ -191,3 +191,17 @@ document.addEventListener("DOMContentLoaded", function() {
         showPage(1);
     }
 });
+
+    // ===========================
+        // 功能五：阅读进度条
+        // ===========================
+        window.addEventListener('scroll', () => {
+            const scrollTop = document.documentElement.scrollTop || document.body.scrollTop;
+            const scrollHeight = document.documentElement.scrollHeight - document.documentElement.clientHeight;
+            const scrolled = (scrollTop / scrollHeight) * 100;
+            
+            const progressBar = document.getElementById('progress-bar');
+            if(progressBar) {
+                progressBar.style.width = scrolled + "%";
+            }
+        });
